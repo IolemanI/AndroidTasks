83c3470a4032e0cd6a3435463b9d9f705d41641c
OleMan95 <aom-95@live.com>
2017-07-13 15:15:21 +0300

    Log and Toast lesson.

--- app/src/main/java/com/oleman/androidtasks/MainActivity.java
+++ app/src/main/java/com/oleman/androidtasks/MainActivity.java
@@ -3,12 +3,18 @@ package com.oleman.androidtasks;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int LAYOUT = R.layout.activity_main;
+    public static final String TAG = "myLogs";
 
     Button task1Btn;
     Button task2Btn;
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
 
+        Log.d(TAG, "Find the view-elements.");
         task1Btn = (Button) findViewById(R.id.task1Btn);
         task2Btn = (Button) findViewById(R.id.task2Btn);
 
@@ -34,9 +41,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.task1Btn:
                 Intent intent = new Intent(this, Task1Activity.class);
                 startActivity(intent);
+                Log.d(TAG, "Open Task #1.");
                 break;
             case R.id.task2Btn:
                 task2Btn.setText("Clicked");
+                Log.d(TAG, "Button TASK 2 clicked.");
+                Toast toast = Toast.makeText(MainActivity.this, "Button TASK 2 was pressed!", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                LinearLayout toastImage = (LinearLayout) toast.getView();
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(R.drawable.toast_image);
+                toastImage.addView(imageView, 0);
+                toast.show();
                 break;
         }
     }
